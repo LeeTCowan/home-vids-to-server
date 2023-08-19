7510f9b09ceefff34e2532a07c7afc17f9240565
Lee Cowan <LeeTCowan@Gmail.com>
2023-08-19 12:36:34 -0400

    Added gitignore

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from modules.transfer_videos_off_phone import transfer_files
 from modules.convert_file_names import FileNameConverter
 from modules.hand_brake_video_processer import HBVideoProcessor
 # from modules. import 
+
+# transfer_files(src, dest, types)
 
 obj = FileNameConverter("something")
 
