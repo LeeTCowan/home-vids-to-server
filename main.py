97e9b68bfc18ca16f839bfeb249cd0ecaa20087a
Lee Cowan <LeeTCowan@gmail.com>
2023-12-25 14:52:39 -0500

    Incomplete changes. Blind commit.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from modules.transfer_videos_off_phone import transfer_files
-from modules.convert_file_names import FileNameConverter
-from modules.hand_brake_video_processer import HBVideoProcessor
-# from modules. import 
+from utils.transfer_videos_off_phone import transfer_files
+from utils.convert_file_names import FileNameConverter
+# from utils.hand_brake_process_videos import HBVideoProcessor
+from utils.get_user_data import get_user_config
+from utils.file_ops import create_clean_working_dir
 
+WORKING_DIR = "temp"
+
+
+create_clean_working_dir(WORKING_DIR)
 # transfer_files(src, dest, types)
 
-obj = FileNameConverter("something")
+# obj = FileNameConverter("something")
 
-obj.print_something("hello")
+config_data = get_user_config()
+
+print(config_data)
+
+# transfer_files(config_data["phone_video_directory"], working_dir, config_data["file_types"])
